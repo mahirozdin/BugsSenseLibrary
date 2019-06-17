8dbc3c5a63b4d6c0408c6bcba0c613b27b2f5afe
Mahir Taha Özdin <ozdinmt@gmail.com>
2019-06-18 02:12:45 +0300

    dfghj

---
+++ app/src/main/java/com/videodownloader/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.videodownloader.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bugssense.crashreports.ExceptionHandler;
+import com.bugssense.crashreports.FeedbackHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FeedbackHandler fh = new FeedbackHandler();
+        fh.sendFeedback("denememe","ramak",this);
+        Thread.UncaughtExceptionHandler defaultUEH = Thread.getDefaultUncaughtExceptionHandler();
+        if (!(defaultUEH instanceof ExceptionHandler)) {
+            Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(defaultUEH,MainActivity.this));
+        }
+
+        int a = 1/0;
+
+    }
+}
